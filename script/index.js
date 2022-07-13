896c7c4924fd4d155d75ffbe3c45a81f8f993e55
Pavel Maksimov <maksps@yandex.ru>
2022-07-14 01:44:05 +0400

    feat: добавить автоматическое добавление карточек

--- script/index.js
+++ script/index.js
@@ -1,12 +1,58 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let exitButton = document.querySelector('.popup__btn-exit');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let input = document.querySelector('.input');
-let nameInput = input.querySelector('.input__text_type_name');
-let jobInput = input.querySelector('.input__text_type_job');
-let formElement = document.querySelector('.popup__form');
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const exitButton = document.querySelector('.popup__btn-exit');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const input = document.querySelector('.input');
+const nameInput = input.querySelector('.input__text_type_name');
+const jobInput = input.querySelector('.input__text_type_job');
+const formElement = document.querySelector('.popup__form');
+const elements = document.querySelector('.elements');
+
+
+function createElement(name, link) {
+  const template = document.querySelector('.element-template').content.querySelector('.element').cloneNode(true);
+  template.querySelector('.element__image').src = link;
+  template.querySelector('.element__image').alt = name;
+  template.querySelector('.element__text').textContent = name;
+  elements.appendChild(template);
+
+}
+
+function createInitialElements() {
+  const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  initialCards.forEach(function (item) {
+    createElement(item.name, item.link)
+  });
+
+}
+
 
 function openPopup() {
   nameInput.value = profileName.textContent;
@@ -25,7 +71,7 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
-
+createInitialElements();
 editButton.addEventListener('click', openPopup);
 exitButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
