b166066ec5d731b6d9a4bfe7bcec3e93ec50a68e
Pavel Maksimov <maksps@yandex.ru>
2022-08-01 23:12:50 +0400

    refactor: добавлены селекторы классов в код

--- script/validate.js
+++ script/validate.js
@@ -1,65 +1,51 @@
 const formRegister = {
-  inputError: 'input__text_type_error',
-  errorTextActive: 'popup__input-error_active',
-  inputText: '.input__text',
-  submitButton: '.popup__btn-save',
-  form: '.popup__form',
-  submitButtonDisable: 'popup__btn-save_inactive'
+  formSelector: '.popup__form',
+  inputSelector: '.input__text',
+  submitButtonSelector: '.popup__btn-save',
+  inactiveButtonClass: 'popup__btn-save_inactive',
+  inputErrorClass: 'input__text_type_error',
+  errorClass: 'popup__input-error_active'
 }
 
-
-const showInputError = (form, inputElement, errorMessage) => {
-  const errorElement = form.querySelector(`.${inputElement.name}-error`);
-  inputElement.classList.add(formRegister.inputError);
+const showInputError = (formSelector, inputElement, errorMessage) => {
+  const errorElement = formSelector.querySelector(`.${inputElement.name}-error`);
+  inputElement.classList.add(formRegister.inputErrorClass);
   errorElement.textContent = errorMessage;
-  errorElement.classList.add(formRegister.errorTextActive);
+  errorElement.classList.add(formRegister.errorClass);
 };
 
-const hideInputError = (form, inputElement) => {
-  const errorElement = form.querySelector(`.${inputElement.name}-error`);
-  inputElement.classList.remove(formRegister.inputError);
-  errorElement.classList.remove(formRegister.errorTextActive);
+const hideInputError = (formSelector, inputElement) => {
+  const errorElement = formSelector.querySelector(`.${inputElement.name}-error`);
+  inputElement.classList.remove(formRegister.inputErrorClass);
+  errorElement.classList.remove(formRegister.errorClass);
   errorElement.textContent = '';
 };
 
-const checkInputValidity = (form, inputElement) => {
+const checkInputValidity = (formSelector, inputElement) => {
   if (!inputElement.validity.valid) {
-    showInputError(form, inputElement, inputElement.validationMessage);
+    showInputError(formSelector, inputElement, inputElement.validationMessage);
   } else {
-    hideInputError(form, inputElement);
+    hideInputError(formSelector, inputElement);
   }
 };
 
-const setEventListeners = (form) => {
-  const inputList = Array.from(form.querySelectorAll(formRegister.inputText));
-  const buttonElement = form.querySelector(formRegister.submitButton);
+const setEventListeners = (formSelector) => {
+  const inputList = Array.from(formSelector.querySelectorAll(formRegister.inputSelector));
+  const buttonElement = formSelector.querySelector(formRegister.submitButtonSelector);
   toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
-      checkInputValidity(form, inputElement);
+      checkInputValidity(formSelector, inputElement);
       toggleButtonState(inputList, buttonElement);
     });
   });
 
 };
 
-// const enableValidation = () => {
-//   const formList = Array.from(document.querySelectorAll('.body'));
-//   formList.forEach((formElement) => {
-//     formElement.addEventListener('submit', function (evt) {
-//       evt.preventDefault();
-//     });
-//     const fieldsetList = Array.from(formElement.querySelectorAll('.popup__form'));
-//     fieldsetList.forEach((fieldSet) => {
-//   setEventListeners(fieldSet);
-// });
-//   });
-// };
-
 const enableValidation = () => {
-  const forms = Array.from(document.querySelectorAll(formRegister.form));
-  forms.forEach((form) => {
-    setEventListeners(form);
+  const forms = Array.from(document.querySelectorAll(formRegister.formSelector));
+  forms.forEach((formSelector) => {
+    setEventListeners(formSelector);
   });
 };
 
@@ -71,9 +57,9 @@ const hasInvalidInput = (inputList) => {
 };
 const toggleButtonState = (inputList, buttonElement) => {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add(formRegister.submitButtonDisable);
+    buttonElement.classList.add(formRegister.inactiveButtonClass);
   }
-  else { buttonElement.classList.remove(formRegister.submitButtonDisable); }
+  else { buttonElement.classList.remove(formRegister.inactiveButtonClass); }
 }
 
 
