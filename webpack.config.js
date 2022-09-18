5f8080a952c1e8112c7c9c49e3ceb82a66ef11d3
Pavel Maksimov <maksps@yandex.ru>
2022-09-18 23:17:14 +0400

    devserver

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
     filename: 'main.js',
     publicPath: ''
   },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+
+    open: true // сайт будет открываться сам при запуске npm run dev
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
