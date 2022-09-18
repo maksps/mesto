168c3e69b02a8facf73a02a8060d7319bfcae5df
Pavel Maksimov <maksps@yandex.ru>
2022-09-18 18:53:17 +0400

    html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 
 const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: { main: './src/script/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
         publicPath: ''
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' 
+    }),
+  ] 
 }
+
 
