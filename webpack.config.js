6e86c8adf1ff064eec280be1d2d114d6b0acdd1d
Pavel Maksimov <maksps@yandex.ru>
2022-09-18 22:39:58 +0400

    cssprefix

--- webpack.config.js
+++ webpack.config.js
@@ -45,9 +45,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        },]
+        use: [MiniCssExtractPlugin.loader,
+           { loader: 'css-loader',
+           options: { importLoaders: 1 }},
+          'postcss-loader'
+          ]
       }
     ]
   }
